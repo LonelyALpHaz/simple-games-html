4ea2e23ce60c5e17ec75ea75045d3342099c37cf
Arthur D <dantas.arthur.contact@gmail.com>
2023-06-20 11:33:11 -0300

    javascript adicionado

---
+++ site/javascript.js
@@ -0,0 +1,25 @@
+const cells = document.querySelectorAll('.hash');
+
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        cell.classList.add('selected');
+    });
+
+    cell.addEventListener('mouseout', () => {
+        cell.classList.remove('selected');
+    });
+});
+
+const playerChoices = document.querySelectorAll('.hash');
+
+    playerChoices.forEach(function(choice) {
+        choice.addEventListener('click', function() {
+            choice.textContent = 'X';
+        })
+    })
+
+const board = [
+    [], [], [],
+    [], [], [],
+    [], [], []
+];
