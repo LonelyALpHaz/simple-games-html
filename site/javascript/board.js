a5bafe16318a68b695341d6f47434ab15e585bec
Arthur D <dantas.arthur.contact@gmail.com>
2023-07-20 15:50:31 -0300

    Pequenas mudanças nas páginas dos jogos

---
+++ site/javascript/board.js
@@ -0,0 +1,6 @@
+function iniciar() {
+    var board = document.getElementById('board')
+    var btn = document.getElementById('btn')
+    btn.style.display = 'none'
+    board.style.display = 'block'
+}
